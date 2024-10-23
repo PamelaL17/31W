3be3e7e3e7c632c4f1ffee61741b3a7fb58fb0e7
Pamela Limoges <pamelalimoges1@gmail.com>
2024-10-23 15:27:08 -0400

    s3c1 amélioration de l'entete, tâches à réaliser pour l'exercice 2

--- README.md
+++ README.md
@@ -9,6 +9,10 @@
 
 ## Exercice-2
 
-### Objectif : Réaliser un thème de base 
+### Objectif : Réaliser un thème de base
+
+- Améliorer la structure Sass
+- Créer une page d'accueil avec front-page.php
+
 - Auteur : Pamela Limoges
-- Description :
+- Adresse URL du site sur WHC : https://gftnth00.mywhc.ca/31w07/
--- footer.php
+++ footer.php
@@ -1,10 +1,17 @@
 <footer class="pied">
-    <section class="global">
+    <section class="global pied__global">
       <div>
         Auteur : Pamela Limoges
+        lien github : https://github.com/PamelaL17/31W/tree/master
+        lien vers WHC : https://gftnth00.mywhc.ca/31w07/
       </div>
       <div>
-        https://gftnth00.mywhc.ca/31w07/
+        Texte résumé sur l'objectif de l'exercice
+        et une image
+      </div>
+      <div>
+        zone de recherche
+        menu sur une colonne
       </div>
     </section>
   </footer>
