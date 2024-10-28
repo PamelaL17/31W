a46390d23a7b85efad4e3a649cb120f1d475146b
Pamela Limoges <pamelalimoges1@gmail.com>
2024-10-28 19:29:08 -0400

    Modification du footer.php et ajout du _pied.scss

--- footer.php
+++ footer.php
@@ -1,18 +1,18 @@
 <footer class="pied">
     <section class="global pied__global">
-      <div>
-        Auteur : Pamela Limoges
-        lien github : https://github.com/PamelaL17/31W/tree/master
-        lien vers WHC : https://gftnth00.mywhc.ca/31w07/
-      </div>
-      <div>
-        Texte résumé sur l'objectif de l'exercice
-        et une image
-      </div>
-      <div>
-        zone de recherche
-        menu sur une colonne
-      </div>
+      <div class="pied__colonne">
+        <h4>31W</h4>
+        <p>Auteur: Pamela Limoges</p>
+        <p><a href="https://github.com/PamelaL17/31W/tree/master" target="_blank">GitHub</a></p>
+        <p><a href="https://gftnth00.mywhc.ca/31w07/" target="_blank">WHC</a></p>
+    </div>
+    <div class="pied__colonne">
+      <?php wp_nav_menu(array('menu' => 'principal', 'container' => 'nav', 'container_class' => 'menu-principal')); ?>
+      <?php get_search_form(); ?>
+    </div>
+    <div class="pied__colonne">
+      <img src="image.jpg" alt="Description de l'image">
+    </div>
     </section>
   </footer>
   <?php wp_footer(); ?>
