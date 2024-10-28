64b1d253cb7689091d5b2f66a4ad3ac15ec169f7
Pamela Limoges <pamelalimoges1@gmail.com>
2024-10-28 19:47:18 -0400

    Modification du fichier footer.php et de _pied.scss et ajout de l'image

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
     <section class="global pied__global">
       <div class="pied__colonne">
         <h4>31W</h4>
+        <h5>Exercice 2</h5>
         <p>Auteur: Pamela Limoges</p>
         <p><a href="https://github.com/PamelaL17/31W/tree/master" target="_blank">GitHub</a></p>
         <p><a href="https://gftnth00.mywhc.ca/31w07/" target="_blank">WHC</a></p>
@@ -11,7 +12,7 @@
       <?php get_search_form(); ?>
     </div>
     <div class="pied__colonne">
-      <img src="image.jpg" alt="Description de l'image">
+      <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo-transparent-png.png" alt="logo">
     </div>
     </section>
   </footer>
